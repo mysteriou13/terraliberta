7d98856a19d06c377591fe858c9c4c1c79d5300d
mysteriou <elio007@hotmail.com>
2018-04-25 10:57:03 +0200

    ajout corps html dans onglet.php

---
+++ onglet.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Un syst&egrave;me d'onglet en html</title>
+    <link type="text/css" rel="stylesheet" media="all" title="CSS" href="style.css" />
+</head>
+<body>
+        <div class="onglets_html">
+        <div class="onglets">
+            <div class="onglet_y onglet"><a href="quoi.html">Quoi</a></div>
+            <div class="onglet_n onglet"><a href="qui.html">Qui</a></div>
+            <div class="onglet_n onglet"><a href="pourquoi.html">Pourquoi</a></div>
+        </div>
+        <div class="contenu">
+            <h1>Quoi?</h1>
+            Un simple syst&egrave;me d'onglet utilisant les technologies:<br />
+            <ul>
+                <li>(X)html</li>
+                <li>CSS</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
+
+
