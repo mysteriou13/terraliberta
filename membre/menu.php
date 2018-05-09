0a08dc476683066f12dcc391000c364b1f771d65
mysteriou <elio007@hotmail.com>
2018-05-09 16:00:38 +0200

    affichage des lien dans ./menu/menu.php

--- membre/menu.php
+++ membre/menu.php
@@ -2,4 +2,20 @@
 
 <div id = "menu">
 menu
+
+<?php
+
+$affiche1 = "SELECT url FROM url WHERE  pseudo = '$pseudo'";
+
+$affiche2 = $mysqli->query($affiche1);
+
+while($affiche3 = $affiche2->fetch_array(MYSQLI_ASSOC)){
+
+echo $affiche3['url'];
+
+echo "</br>";
+
+}
+
+ ?>
 </div>
