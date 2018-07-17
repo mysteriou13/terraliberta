b6c3c4f63e244df2f9c4f79c94ead8c2e8acf6f9
mysteriou <elio007@hotmail.fr>
2018-07-17 21:50:01 +0200

    surpresssion iframe dans index.php

--- index.php
+++ index.php
@@ -11,9 +11,7 @@ $d = "ff";
 ?>
 <html>
 
-<iframe style = "display:none;"src = "http://s708280615.onlinehome.fr">
 
-</iframe>
 
 
 <title> terraliberta </title>		
---
+++ install/installcons.php
@@ -0,0 +1,6 @@
+<?php
+ const  server  = "localhost";
+ const  data  = "etnc";
+ const  login  = "root";
+ const  pass  = "elio13790@";
+ ?>
