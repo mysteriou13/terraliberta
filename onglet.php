1c64d86e8e28c85b82faf2847522748d8927d0a0
mysteriou <elio007@hotmail.fr>
2018-08-08 16:40:47 +0200

    change desgin onglet

--- onglet.php
+++ onglet.php
@@ -16,7 +16,9 @@
         </div>
         <div id = "c1" class="contenu">
 
-        <iframe id  = "f" style  = "border:1px solid; height:100%; width:90%;">
+        <iframe id  = "f" style  = "border:1px solid cyan; height:100%; width:90%;
+   background-color:white;
+ ">
         </iframe>
 
       </div>
