f97515dcf7b2f9664ac22aacc3a42b817866715e
mysteriou <eli007@hotmail.fr>
2018-12-25 11:17:09 +0100

    include le header de vecchionet dans ./index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <?php 
-
+include("/var/www/html/vecchionet.com/header.php");
 session_start();
 
 $f = "./install/installcons.php";
